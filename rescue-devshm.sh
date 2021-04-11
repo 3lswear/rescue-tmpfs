c04707b202160e6546bfe7e862dc18130071073b
3lswear <r.malyavsky@protonmail.com>
2021-04-12 02:39:54 +0300

    fixed

--- rescue-devshm.sh
+++ rescue-devshm.sh
@@ -2,10 +2,11 @@
 OUT_DIR=/media/linux_data/data/rescued
 BAK_DIR=$(date +%F' '%R)
 DIR="$OUT_DIR"/"$BAK_DIR"
-mkdir "$DIR"
-if [ "$(ls -A $DIR)" ]
+if [ "$(ls -A /dev/shm/)" ]
 then
+	mkdir "$DIR"
 	cp -pru /dev/shm/* "$DIR"
+	echo "done!"
 else
 	echo "directory empty"
 fi
